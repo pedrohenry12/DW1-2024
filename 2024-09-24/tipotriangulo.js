92a81dfe838c2bf5ec8b9adfc8e1830e8073ef2b
pedrohenry12 <faquinipedro12@gmail.com>
2024-09-24 16:32:23 -0300

    Update tipotriangulo.js

--- 2024-09-24/tipotriangulo.js
+++ 2024-09-24/tipotriangulo.js
@@ -1,13 +1,24 @@
 
-function ehTriangulo(lado1, lado2, lado3){
+function tipoTriangulo(lado1, lado2, lado3){
     if ((lado1 + lado2) > lado3 && (lado1 + lado3) > lado2 && (lado3 + lado2) > lado1){
-        console.log("Não forma triangulo")
-        else if{console.log("Não forma triangulo")}
+        if((lado1 = lado2) || (lado2 = lado3) || (lado1 = lado3)){
+            console.log("triangulo equilatero")
+        }
+        else if((lado1 = lado2) && (lado1 = lado3) && (lado2 = lado3)){
+            console.log("triangulo equilátero")
+        }
+        else if((lado1 =! lado2) && (lado2 =! lado3) && (lado1 =! lado3)){
+            console.log("triangulo escaleno")
+        }
     }
-    
-
+    else{
+        console.log("Não forma triangulo")
+    }
 }
 
-let l1 = 10
+let l1 = 20
 let l2 = 20
-let l3 = 15
+let l3 = 20
+
+tipoTriangulo(l1, l2, l3)
+
