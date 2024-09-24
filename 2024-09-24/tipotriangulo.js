171a11296002ae265908c96511a47b5fd1f8e9f5
pedrohenry12 <faquinipedro12@gmail.com>
2024-09-24 13:56:07 -0300

    Create tipotriangulo.js

---
+++ 2024-09-24/tipotriangulo.js
@@ -0,0 +1,13 @@
+
+function ehTriangulo(lado1, lado2, lado3){
+    if ((lado1 + lado2) > lado3 && (lado1 + lado3) > lado2 && (lado3 + lado2) > lado1){
+        console.log("Não forma triangulo")
+        else if{console.log("Não forma triangulo")}
+    }
+    
+
+}
+
+let l1 = 10
+let l2 = 20
+let l3 = 15
