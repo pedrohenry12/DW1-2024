6dde3d5e46c78264c0afbf68e905fadcff185ba1
pedrohenry12 <faquinipedro12@gmail.com>
2024-09-27 21:56:23 -0300

    Create bissexto.js

---
+++ 2024-09-24/bissexto.js
@@ -0,0 +1,12 @@
+function bissexto(a){
+    if(a%400 == 0 || a%4 == 0){
+        console.log("Esse ano é bissexto")
+    }
+    else{
+        console.log("Esse ano não é bissexto")
+    }
+}
+
+let ano = 1000
+
+bissexto(ano)
