a92f86a518006a2fdaa92cf764c76bc0b947ee6d
pedrohenry12 <faquinipedro12@gmail.com>
2024-11-14 09:23:21 -0300

    .

---
+++ 2024-11-14/revisao.js
@@ -0,0 +1,30 @@
+const vet = [10,15,20,25,30,35]
+
+//const copia = []
+
+//for(elemento of vet){
+ //   copia.push(elemento * 10)
+//}
+
+
+//function multiplica10(elemento){
+//    return elemento*10
+//}
+//const copia = vet.map(multiplica10)
+
+//const copia = vet.map(function (elemento){
+ //   return elemento*10
+//})
+
+// => arrow function: funções simples, podem ser escritas em uma unica linha:
+// const multiplica3 = elemento => elemento * 3
+const multiplica3 = (elemento) => {
+    return elemento * 3
+}
+
+
+
+
+
+
+console.log(copia)
