deb9594e10bb1707ba79bc0f23ff860e48beaca8
pedrohenrt12 <faquinipedro12@gmail.com>
2025-05-26 09:17:57 -0300

    .

---
+++ dw2/scipt.js
@@ -0,0 +1,28 @@
+const button = document.getElementById("botao")
+
+button.addEventListener("click", () => {
+  const isbn = document.getElementById("idLivro").value.trim()
+  
+  if (!isbn) {
+    alert("Digite um ISBN válido.")
+    return
+  }
+
+  const url = `https://brasilapi.com.br/api/isbn/v1/${isbn}`
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Livro não encontrado")
+      }
+      return response.json();
+    })
+    .then(data => {
+      document.getElementById("titulo").textContent = data.title
+      document.getElementById("autor").textContent = data.authors.join(', ')
+      document.getElementById("ano").textContent = data.year
+    })
+    .catch(error => {
+      alert("Erro ao buscar o livro: " + error.message)
+    })
+})
