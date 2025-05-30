239906c308df34d936ad4695e72f13c711fb98b2
pedrohenry12 <faquinipedro12@gmail.com>
2025-05-30 09:59:43 -0300

    .

---
+++ aula_30_01/src/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header(){
+    return(
+        
+            <header className="app-header">
+                <h1>Bem-vindo patrão!</h1>
+            </header>
+        
+    )
+}
+
+export default Header
